d1be5c862fc7b36c182c39851c8d54c0741be316
夏子谆 <68000706+dundunnp@users.noreply.github.com>
2021-11-14 22:19:21 +0800

    Update 南大家园自动打卡.js

--- 南大家园自动打卡.js
+++ 南大家园自动打卡.js
@@ -1,5 +1,21 @@
 auto.waitFor()
 
+var { password } = hamibot.env;
+var { select } = hamibot.env;
+
+function number_click(num) {
+    var number = text(num).findOne().bounds();
+    click(number.centerX(), number.centerY());
+}
+
+function scroll() {
+    if (select == 'a') {
+        swipe(500, 1700, 500, 300, 10);
+    } else {
+        swipe(500, 300, 500, 1700, 10);
+    }
+}
+
 function back_home() {
     while (!text('通讯录').exists()) {
         id('eh').click();
@@ -20,6 +36,35 @@ function mark() {
     }
 }
 
+function input_password() {
+    for (var i = 0; i < password.length; i++) {
+        switch(password[i]) {
+            case '1': number_click('1'); break;
+            case '2': number_click('2'); break;
+            case '3': number_click('3'); break;
+            case '4': number_click('4'); break;
+            case '5': number_click('5'); break;
+            case '6': number_click('6'); break;
+            case '7': number_click('7'); break;
+            case '8': number_click('8'); break;
+            case '9': number_click('9'); break;
+            case '0': number_click('0'); break;
+            default: toast("密码输入错误");
+                exit();
+        }
+    }
+}
+
+while (!device.isScreenOn()) {
+    device.wakeUpIfNeeded();
+    scroll();
+    if (password) {
+        while (!text('1').exists()) scroll();
+        input_password();
+    }
+}
+
+home();
 app.launchApp('微信');
 
 back_home();
@@ -29,7 +74,6 @@ back_home();
 // while (!click('a学生疫情常态化管理'));
 my_click('通讯录');
 my_click('南昌大学');
-// 这里不知道为什么my_click会出现bug跳到公众号去
 while(!click('a学生疫情常态化管理'));
 
 while (!text('用户无权访问').exists() && !text('每日健康打卡').exists());
